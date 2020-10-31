0b7ba3392df5687b37585ece647a2ccb901f25bb
Will Studabaker <will.studabaker@gmail.com>
2020-10-31 14:25:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,10 +31,10 @@ class Song
     @@all.detect {|song| song.name == name}
   end
   def self.find_or_create_by_name (name)
-    if self.find_by_name == nil
-      self.create_by_name
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
     else
-      self.find_by_name
+      self.find_by_name(name)
     end
   end
 end
